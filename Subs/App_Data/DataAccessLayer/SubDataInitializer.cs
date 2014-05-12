5eec8e01a898fbd3341178af3552d4c94d6b59ca
freyr19 <freyr1980@hotmail.com>
2014-05-12 16:15:06 +0000

    EWtrytulyiæiÆO

--- Subs/App_Data/DataAccessLayer/SubDataInitializer.cs
+++ Subs/App_Data/DataAccessLayer/SubDataInitializer.cs
@@ -8,38 +8,38 @@ using Subs.Models.Entity;
 
 namespace Subs.App_Data.DataAccessLayer
 {
-    public class SubDataInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<SubDataContext>
-    {
-        protected override void Seed(SubDataContext context)
-        {
-            var clients = new List<Client>
-            {
-                new Client
-                {
-                    sUsername = "Guffi",
-                    sPass = "meistari",
-                    sEmail = "guffi@ru.is",
-                    iRanking = 1,
-                    iTheme = 1,
-                    dSignupDate = DateTime.Parse("07-05-2014")
-                } //,
-            };
+    //public class SubDataInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<SubDataContext>
+    //{
+    //    protected override void Seed(SubDataContext context)
+    //    {
+    //        var clients = new List<Client>
+    //        {
+    //            new Client
+    //            {
+    //                sUsername = "Guffi",
+    //                sPass = "meistari",
+    //                sEmail = "guffi@ru.is",
+    //                iRanking = 1,
+    //                iTheme = 1,
+    //                dSignupDate = DateTime.Parse("07-05-2014")
+    //            } //,
+    //        };
 
-            clients.ForEach(c => context.Clients.Add(c));
-            context.SaveChanges();
+    //        clients.ForEach(c => context.Clients.Add(c));
+    //        context.SaveChanges();
 
-            var comments = new List<Comment>
-            {
-                new Comment
-                {
-                    iCommentId = 1,
-                    sCommentText = "Geðveikt",
-                    dCommentDate = DateTime.Parse("07-05-2014")
-                }
-            };
+    //        var comments = new List<Comment>
+    //        {
+    //            new Comment
+    //            {
+    //                iCommentId = 1,
+    //                sCommentText = "Geðveikt",
+    //                dCommentDate = DateTime.Parse("07-05-2014")
+    //            }
+    //        };
 
-            comments.ForEach(c => context.Comments.Add(c));
-            context.SaveChanges();
-        }
-    }
+    //        comments.ForEach(c => context.Comments.Add(c));
+    //        context.SaveChanges();
+    //    }
+   // }
 }
