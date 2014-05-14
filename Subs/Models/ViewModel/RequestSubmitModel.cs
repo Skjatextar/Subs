524cc0fd56e6cb1199942f6b4d7d23f6400e593c
SveinbjornB <Sveimbi@gmail.com>
2014-05-14 11:41:39 +0000

    stuff

---
+++ Subs/Models/ViewModel/RequestSubmitModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Subs.App_Data.DataAccessLayer;
+
+namespace Subs.Models.ViewModel
+{
+    public class RequestSubmitModel
+    {
+
+        public string RequestTitel { get; set; }
+       // public bool RequestLanguage { get; set; }
+       // public bool RequstType { get; set; }
+        public string RequestDescription { get; set; }
+    }
+}
